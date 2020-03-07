bcd9e40d5ded013b0ad0a199f2edd96a8eeae2e6
Sidney Alcantara <sidney@sidney.me>
2020-03-08 00:34:11 +1100

    restructure for full access to Algolia search API

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,25 @@
 import { useReducer, useMemo, useCallback, useEffect } from 'react';
 import algoliasearch from 'algoliasearch/lite';
+import { RequestOptions } from '@algolia/transporter';
 import { SearchOptions, SearchResponse } from '@algolia/client-search';
 
 /**
  * Creates the Algolia search client and initialises the specified index.
- * @param indexName Index to initialise
+ * @param appId Algolia app ID
  * @param searchKey API key to search this index
+ * @param indexName Index to initialise
  */
-export const createAlgoliaIndex = (indexName: string, searchKey: string) =>
-  algoliasearch(process.env.REACT_APP_ALGOLIA_APP_ID!, searchKey).initIndex(
-    indexName
-  );
+export const createAlgoliaIndex = (
+  appId: string,
+  searchKey: string,
+  indexName: string
+) => algoliasearch(appId, searchKey).initIndex(indexName);
 
+/** Current request state, hits retrieved, and loading status. */
 interface SearchState {
-  /** Search query text used in Algolia call */
-  search: NonNullable<SearchOptions['query']>;
-  /** Facet filters. See https://www.algolia.com/doc/api-reference/api-parameters/facetFilters/ */
-  filters: SearchOptions['facetFilters'];
-  /** Algolia search response object — contains one page of hits at a time */
-  result: SearchResponse<any> | null;
-  /** Contains all hits when more results are called */
+  /** Algolia SearchResponse object — contains only last page of hits retrieved */
+  response: SearchResponse<any> | null;
+  /** Contains all hits for search query, including all pages retrieved */
   hits: SearchResponse<any>['hits'];
   /** Set when loading initially or loading more hits */
   loading: boolean;
@@ -27,98 +27,111 @@ interface SearchState {
   hasMore: boolean;
 }
 
-interface SearchAction extends Partial<SearchState> {
-  type?: 'MORE_HITS' | '';
-}
-
 /**
- * Search reducer to shallow-merge updates to state. Also handles checking if
- * there are more pages that can be queried and concatenating hits when more
- * hits are received.
- * @param prevState See SearchState interface
- * @param action Contains updates to state. Use type: 'MORE_HITS' to concatenate hits
+ * Updates hook’s internal `SearchState`. Handles:
+ * - Checking if there are more pages that can be retrieved
+ * - Concatenating hits if we queried a new page
+ * @param prevState See `SearchState` interface
+ * @param updates Updates to `SearchState`
  */
-const searchReducer = (prevState: SearchState, action: SearchAction) => {
-  const { type, ...updates } = action;
+const searchReducer = (
+  prevState: SearchState,
+  updates: Partial<SearchState>
+) => {
+  const gotMore = updates?.response?.page && updates?.response?.page > 0;
 
   const hits =
-    type === 'MORE_HITS' && updates.result
-      ? [...prevState.hits, ...updates.result.hits]
-      : updates.hits ?? updates.result?.hits ?? prevState.hits ?? [];
+    gotMore && updates.response
+      ? [...prevState.hits, ...updates.response.hits]
+      : // If we’re not getting results from a new page, `hits` is set to:
+        // 1. overwrite `hits` from `updates` (when resetting query),
+        // 2. use the latest `hits` from the response,
+        // 3. use `hits` from the previous state, or
+        // 4. an empty array
+        updates.hits ?? updates.response?.hits ?? prevState.hits ?? [];
 
-  const hasMore = updates.result
-    ? updates.result.page < updates.result.nbPages - 1
+  const hasMore = updates.response
+    ? updates.response.page < updates.response.nbPages - 1
     : false;
 
-  return { ...prevState, ...updates, hasMore, hits };
+  return { ...prevState, ...updates, hits, hasMore };
 };
 
 /**
- * Hook to make Algolia queries with search term, facet filters, and pagination.
- * @param indexName Algolia index to query
+ * Hook to make Algolia search queries with built-in support for pagination.
+ * @param appId Algolia app ID
  * @param searchKey API key to search the index
- * @param initialState Initial state for search query and filters
+ * @param indexName Algolia index to query
+ * @param initialRequest Initial `SearchOptions` object sent to Algolia request
  * @returns The following array:
- *   1. `searchState` (containing `hits` and `loading`),
- *   2. `searchDispatch` (to update `search` and `filters` in state), and
+ *   1. `searchState`: containing `hits` and `loading`,
+ *   2. `requestDispatch` to update `SearchOptions` passed to Algolia — see
+ *      https://www.algolia.com/doc/api-reference/search-api-parameters/, and
  *   3. `getMore` to get the next page of results
  */
 export default function useAlgolia(
-  indexName: string,
+  appId: string,
   searchKey: string,
-  initialState?: Pick<SearchState, 'search' | 'filters'>
+  indexName: string,
+  initialRequest: RequestOptions & SearchOptions = {}
 ) {
+  // Stores response status
   const [searchState, searchDispatch] = useReducer(searchReducer, {
-    search: '',
-    filters: [],
-    ...initialState,
-    result: null,
+    response: null,
     hits: [],
     loading: false,
     hasMore: false,
   });
 
-  // Get Algolia index — only created for each indexName and searchKey
-  const index = useMemo(() => createAlgoliaIndex(indexName, searchKey), [
-    indexName,
+  // Store the `SearchOptions` request object that can shallow-merge updates
+  const [request, requestDispatch] = useReducer(
+    (
+      prev: RequestOptions & SearchOptions,
+      updates: RequestOptions & SearchOptions
+    ) => ({ ...prev, ...updates }),
+    initialRequest
+  );
+
+  // Get Algolia index — only created for each `appId`, `indexName`, and `searchKey`
+  const index = useMemo(() => createAlgoliaIndex(appId, searchKey, indexName), [
+    appId,
     searchKey,
+    indexName,
   ]);
 
   // Query algolia with search text + filters
+  // Function will be recreated when `SearchOptions` request object changes
   const query = useCallback(
     async (page = 0) => {
-      const getMore = page > 0;
-
-      if (getMore) searchDispatch({ loading: true });
+      if (page > 0) searchDispatch({ loading: true });
+      // If we’re not getting a new page, reset the hits
       else searchDispatch({ loading: true, hits: [] });
 
-      const result = await index.search<any>(searchState.search, {
+      const response = await index.search<any>('', {
         page,
-        facetFilters: searchState.filters,
+        ...request,
       });
 
-      searchDispatch({
-        result,
-        type: page > 0 ? 'MORE_HITS' : '',
-        loading: false,
-      });
+      searchDispatch({ response, loading: false });
     },
-    [index, searchState.filters, searchState.search]
+    [index, request]
   );
 
-  // Get completely new query when search or filters change (reset page)
+  // Get completely new query when `query` function is recreated above
   useEffect(() => {
     query();
-  }, [index, query]);
+  }, [query]);
 
+  // Get more by incrementing the page. Does nothing if we’re still waiting
+  // on new results to arrive or if there are no more pages to be loaded
   const getMore = () => {
-    if (searchState.result && !searchState.loading && searchState.hasMore)
-      query(searchState.result.page + 1);
+    if (searchState.response && !searchState.loading && searchState.hasMore)
+      query(searchState.response.page + 1);
   };
 
-  return [searchState, searchDispatch, getMore] as [
+  return [searchState, requestDispatch, getMore] as [
     typeof searchState,
-    typeof searchDispatch,
+    typeof requestDispatch,
     typeof getMore
   ];
 }
